d16b2cbd79149b739388cb8c802102567b3d91ee
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2024-11-14 09:28:19 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main()
+{
+    float posti, adulti, bambini;
+    posti = 0;
+    bambini = 0;
+    adulti = 0;
+    while (posti<30)
+    {
+        cout << "quanti adulti salgono?" << endl;
+        cin >> adulti;
+        cout << "quanti bambini salgono?" << endl;
+        cin >> bambini;
+        posti = posti+adulti;
+        posti = posti + (bambini/2);
+    }
+    if (posti ==30)
+    {
+        cout << "la giostra è al completo" << endl;
+    }
+    else
+    {
+        cout << "vanno liberati " << posti-30 << " posti" << endl;
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
